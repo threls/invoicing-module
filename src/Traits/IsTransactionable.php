de6f7d43078c49adb8de983eaaaca22581ff36b4
sabina <sabinahaloci@gmail.com>
2025-04-08 00:47:16 +0200

    fix

--- src/Traits/IsTransactionable.php
+++ src/Traits/IsTransactionable.php
@@ -2,11 +2,17 @@
 
 namespace Threls\ThrelsInvoicingModule\Traits;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Threls\ThrelsInvoicingModule\Models\TransactionItem;
 
 trait IsTransactionable
 {
+    public function transactionItems(): MorphMany
+    {
+        return $this->morphMany(TransactionItem::class, 'model');
+    }
+
     public function transactionItem(): MorphOne
     {
         return $this->morphOne(TransactionItem::class, 'model');
