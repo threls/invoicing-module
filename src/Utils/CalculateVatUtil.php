76ee06f6394cf8ff6704fcb2eb36b1dbccfea0ef
sabina <sabinahaloci@gmail.com>
2025-05-11 21:51:10 +0200

    invoice fixes

--- src/Utils/CalculateVatUtil.php
+++ src/Utils/CalculateVatUtil.php
@@ -6,6 +6,6 @@ class CalculateVatUtil
 {
     public function extractVAT(int $totalPrice, int $vatRate): int
     {
-        return (int) round($totalPrice * $vatRate / (100 + $vatRate));
+        return (int) round($totalPrice * $vatRate / (100 + $vatRate),0,PHP_ROUND_HALF_UP);
     }
 }
