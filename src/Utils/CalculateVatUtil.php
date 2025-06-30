929eaff28e2c749543b92ef41c9591ef0b61c24c
sabina <sabinahaloci@gmail.com>
2025-06-30 17:41:34 +0200

    fix calculate vat util

--- src/Utils/CalculateVatUtil.php
+++ src/Utils/CalculateVatUtil.php
@@ -6,6 +6,9 @@ class CalculateVatUtil
 {
     public function extractVAT(int $totalPrice, int $vatRate): int
     {
+        if ($totalPrice == 0){
+            return 0;
+        }
         return (int) round($totalPrice * $vatRate / (100 + $vatRate));
     }
 }
